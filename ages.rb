fc017085eca32b4bfa2b2dccb6d6d7df37fced32
Vineeshau <125856539+Vineeshau@users.noreply.github.com>
2023-02-28 10:24:49 +0530

    Delete ages.rb

--- ages.rb
+++
@@ -1,9 +0,0 @@
-ages = [28, 35, 42, 51, 39, 44, 32, 48, 29, 36]
-total_age = 0
-avg = 0
-ages.each do |data|
-    # puts "values are : #{data}"
-    total_age += data
-    avg = total_age/ages.length
-end
-puts " The average age of the book club members is #{avg}"
