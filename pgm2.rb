4546d91a627407b315ab2331b56790ff87a93111
Vineeshau <Vineesh.AU@chordify.com>
2023-03-01 12:58:57 +0530

    hash pgm2 updated

--- pgm2.rb
+++ pgm2.rb
@@ -1,32 +1,4 @@
 #2.Mike has a list of books that he wants to keep track of. He wants to store the name, author, and year of publication for each book in a hash. He also wants to be able to search for a book by its name and update its year of publication.Help Mike write a Ruby program to solve this problem using hash operations.
-# flag = 0
-# hash1 = {}
-# while flag != 1
-# 	print "Enter the book key :"
-# 	key = gets.chomp
-# 	print "Enter the book name :"
-# 	name = gets.chomp
-# 	print "Enter the author key :"
-# 	key2 = gets.chomp
-# 	print "Enter the author name :"
-# 	author = gets.chomp
-# 	print "Enter the year key :"
-# 	key3 = gets.chomp
-# 	print "Year of publication :"
-# 	year = gets.chomp
-# 	hash1.store(key, name)
-# 	hash1.store(key2, author)
-# 	hash1.store(key3, year)
-# 	print "Do you want to continue yes/no ?"
-# 	continue = gets.chomp
-# 	if continue == 'no'
-# 		flag = 1
-# 	end
-# 	hash1.map do |key, val|
-# 		puts key
-# 	end
-# 	puts hash1
-# end
 books = {"To Kill a Mockingbird" => { author: "Harper Lee", year: 1960 },"1984" => { author: "George Orwell", year: 1949 },"The Catcher in the Rye" => { author: "J.D. Salinger", year: 1951 },"Pride and Prejudice" => { author: "Jane Austen", year: 1813 },"The Great Gatsby" => { author: "F. Scott Fitzgerald", year: 1925 }}
 print "Enter the book name :"
 name = gets.chomp
