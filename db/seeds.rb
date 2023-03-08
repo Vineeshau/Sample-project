e5c4cb6d02a06e55f08f075cda55e5a3e1888ee5
Vineeshau <Vineesh.AU@chordify.com>
2023-03-08 10:44:40 +0530

    Rails

---
+++ db/seeds.rb
@@ -0,0 +1,24 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+student = Student.create(
+	first_name: 'Vinu',
+	last_name: 'Vineesh',
+	phone: '1234567890',
+	email: 'vinu@gmail.com'
+	)
+
+# 20.times do |i|
+# 	puts "Creating student #{i+1}"
+# 	Student.create(
+# 		first_name: "Student #{i+1}",
+# 		last_name: "Lname #{i+1}",
+# 		phone: "123456789#{i+1}",
+# 		email: "student#{i+1}@bootcamp.com"
+# 	)
+end
