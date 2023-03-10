7685275dbbdf48d3283581c02d6443ed836bcea5
Vineeshau <Vineesh.AU@chordify.com>
2023-03-10 17:08:16 +0530

    Modified student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Student < ApplicationRecord
-  validates :first_name, :last_name, :email, presence: true
+  validates :first_name, :last_name, :email, :country, :value, :image presence: true
   validates :phone, length: { :minimum => 10, :maximum => 15 } 
   validates_format_of :phone, :with =>  /\d[0-9]\)*\z/ 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
